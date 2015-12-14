57dde9b5bf70c0d3a0e5991442c58d9f4bc16b84
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-14 16:58:13 -0500

    create admin sign in helper

---
+++ spec/support/admin_sign_in_helper_spec.rb
@@ -0,0 +1,9 @@
+module AdminSignInHelper
+  def sign_in_as(admin)
+    visit new_admin_session_path
+
+    fill_in "Email", with: admin.email
+    fill_in "Password", with: admin.password
+    click_button "Log in"
+  end
+end
