ed51f028ef12ec1a6d9b02e163829def127d3536
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-14 17:48:34 -0500

    add pet unit test

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Pet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name_of_pet) }
+  it { should_not allow_value(nil).for(:name_of_pet) }
+
+  it { should validate_inclusion_of(:type_of_pet).in_array(["dog", "cat", "bird") }
+
+  it { should validate_length_of(:breed).is_at_most(35) }
+
+  it { should validate_presence_of(:age) }
+  it { should_not allow_value(nil).for(:age) }
+  it { should validate_numericality_of(:age)
+
+  it { should validate_presence_of(:weight) }
+  it { should_not allow_value(nil).for(:weight) }
+
+  it { should validate_presence_of(:name_of_pet) }
+  it { should_not allow_value(nil).for(:name_of_pet) }
 end
