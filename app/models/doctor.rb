4adcce02386947d984bcf4db538808776a765482
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-14 16:50:23 -0500

    fix hound errors

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,7 +1,7 @@
 class Doctor < ActiveRecord::Base
   validates :name, presence: true
-  validates_length_of :name, maximum: 35
-  validates_length_of :zip, maximum: 5
+  validates :name, length: { maximum: 35 }
+  validates :zip, length: { maximum: 5 }
   validates :years_in_practice, numericality: { only_integer: true }
   validates :years_in_practice, numericality: { less_than_or_equal_to: 100 }
   validates :years_in_practice, numericality: { greater_than_or_equal_to: 1 }
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Doctor, type: :model do
   it { should validate_presence_of(:name) }
   it { should_not allow_value(nil).for(:name) }
-  
+
   it { should validate_length_of(:zip).is_at_most(5) }
 
   it { should validate_numericality_of(:years_in_practice).is_greater_than_or_equal_to (1) }
