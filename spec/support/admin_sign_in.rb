c6dc4cdc36eaf1f880b5aa530c2529e6bf6bd316
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-14 17:06:43 -0500

    add factory girl admin to admin sign in helper

--- spec/support/admin_sign_in.rb
+++ spec/support/admin_sign_in.rb
@@ -1,7 +1,8 @@
-module AdminSignInHelper
-  def sign_in_as(admin)
-    visit new_admin_session_path
+module AdminSignIn
+  def admin_sign_in
+    admin = FactoryGirl.create(:admin)
 
+    visit new_admin_session_path
     fill_in "Email", with: admin.email
     fill_in "Password", with: admin.password
     click_button "Log in"
