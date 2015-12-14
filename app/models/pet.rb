06b3d98d622d610d9c93bcf22b3e60e1bbaddb0a
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-14 18:01:56 -0500

    add unit test & model validations

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,2 +1,11 @@
 class Pet < ActiveRecord::Base
+  validates :name_of_pet, presence: true
+  validates :name_of_pet, length: { maximum: 35 }
+  validates :type_of_pet, inclusion: { in: ["cat", "dog", "bird"] }
+  validates :breed, presence: true
+  validates :breed, length: { maximum: 35 }
+  validates :age, presence: true
+  validates :weight, presence: true
+  validates :weight, numericality: { only_float: true }
+  validates :date_of_last_visit, presence: true
 end
--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -4,13 +4,12 @@ RSpec.describe Pet, type: :model do
   it { should validate_presence_of(:name_of_pet) }
   it { should_not allow_value(nil).for(:name_of_pet) }
 
-  it { should validate_inclusion_of(:type_of_pet).in_array(["dog", "cat", "bird") }
+  it { should validate_inclusion_of(:type_of_pet).in_array(["dog", "cat", "bird"]) }
 
   it { should validate_length_of(:breed).is_at_most(35) }
 
   it { should validate_presence_of(:age) }
   it { should_not allow_value(nil).for(:age) }
-  it { should validate_numericality_of(:age)
 
   it { should validate_presence_of(:weight) }
   it { should_not allow_value(nil).for(:weight) }
