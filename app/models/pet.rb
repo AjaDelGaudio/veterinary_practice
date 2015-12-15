4f0f20b8c40df3456caf4dbf02aec195d942e0f6
Aja DelGaudio <ajadelgaudio@gmail.com>
2015-12-14 19:56:56 -0500

    capitalize type_of_pet sections in pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,7 +1,7 @@
 class Pet < ActiveRecord::Base
   validates :name_of_pet, presence: true
   validates :name_of_pet, length: { maximum: 35 }
-  validates :type_of_pet, inclusion: { in: ["cat", "dog", "bird"] }
+  validates :type_of_pet, inclusion: { in: ["Cat", "Dog", "Bird"] }
   validates :breed, presence: true
   validates :breed, length: { maximum: 35 }
   validates :age, presence: true
